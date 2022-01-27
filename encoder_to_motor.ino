57128fd338cbd01800f7dc9983e19ce9fc26465a
meltanrm <89637384+meltanrm@users.noreply.github.com>
2022-01-27 14:18:00 +0000

    Create encoder_to_motor.ino
    initial code: one encoder to one motor

---
+++ encoder_to_motor.ino
@@ -0,0 +1,63 @@
+volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+const int stepPin1 = 7;
+const int dirPin1 = 6;
+
+#define CLK 2
+#define DT 3
+ 
+void setup() {
+  Serial.begin (9600);
+  
+  // declaring pins as encoder outputs 
+  pinMode(CLK, INPUT_PULLUP); // internal pullup input pin 2 
+  pinMode(DT, INPUT_PULLUP); // internalเป็น pullup input pin 3
+
+  // declaring pins as motor outputs 
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  
+   //Setting up interrupt
+  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  attachInterrupt(0, ai0, RISING);
+   
+  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  attachInterrupt(1, ai1, RISING);
+  }
+   
+  void loop() {
+  // Send the value of counter
+    if( counter != temp ){
+      Serial.println (counter);
+      temp = counter;
+    }
+  }
+   
+  void ai0() {
+    // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+    // Check pin 3 to determine the direction
+    if(digitalRead(3)==LOW) {
+      counter++;
+      digitalWrite(dirPin1, HIGH);
+      digitalWrite(stepPin1, HIGH);
+      delayMicroseconds(1000);
+      digitalWrite(stepPin1, LOW);
+      delayMicroseconds(1000);      
+    }else{
+      counter--;
+      digitalWrite(dirPin1, LOW);
+      digitalWrite(stepPin1, LOW);
+      delayMicroseconds(1000);
+      digitalWrite(stepPin1, HIGH);
+      delayMicroseconds(1000);
+    }
+  }
+   
+  void ai1() {
+    // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+    // Check with pin 2 to determine the direction
+    if(digitalRead(2)==LOW) {
+      counter--;
+    }else{
+      counter++;
+    }
+  }
