2dde8a2b352f2d19798517d60e29d86bb6b9fdb3
meltanrm <89637384+meltanrm@users.noreply.github.com>
2022-01-28 14:46:16 +0000

    Update Encoder_to_motor.ino

--- Encoder_to_motor/Encoder_to_motor.ino
+++ Encoder_to_motor/Encoder_to_motor.ino
@@ -1,9 +1,12 @@
 volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
 const int stepPin1 = 7;
 const int dirPin1 = 6;
+const int stepPin1 = 11;
+const int dirPin1 = 10;
 
 #define CLK 2
 #define DT 3
+//ensure you change the attachInterrupt pins as well 
  
 void setup() {
   Serial.begin (9600);
@@ -36,6 +39,9 @@ void setup() {
     // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
     // Check pin 3 to determine the direction
     if(digitalRead(3)==LOW) {
+    // ai0 is activated if DigitalPin nr 18 is going from LOW to HIGH
+    // Check pin 19 to determine the direction
+    if(digitalRead(DT)==LOW) {
       counter++;
       digitalWrite(dirPin1, HIGH);
       digitalWrite(stepPin1, HIGH);
@@ -56,6 +62,9 @@ void setup() {
     // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
     // Check with pin 2 to determine the direction
     if(digitalRead(2)==LOW) {
+    // ai0 is activated if DigitalPin nr 19 is going from LOW to HIGH
+    // Check with pin 18 to determine the direction
+    if(digitalRead(CLK)==LOW) {
       counter--;
     }else{
       counter++;
