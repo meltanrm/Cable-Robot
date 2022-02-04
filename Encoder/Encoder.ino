8aaca962e7453fd1d06e3ba9cc949750572f5b9f
meltanrm <89637384+meltanrm@users.noreply.github.com>
2022-02-04 11:40:10 +0000

    fixed indentation

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,71 +1,43 @@
 volatile long temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
-const int dirPin1 = 6; // motor 1 
-const int stepPin1 = 7;
-const int stepsPerRevolution = 200;
     
 void setup() {
-  pinMode(stepPin1, OUTPUT);
-  pinMode(dirPin1, OUTPUT);
-  
   Serial.begin (9600);
 
-  pinMode(20, INPUT_PULLUP); // internal pullup input pin 2 
+  pinMode(20, INPUT_PULLUP); // internal pullup input pin 20 
   
-  pinMode(21, INPUT_PULLUP); // internal pullup input pin 3
+  pinMode(21, INPUT_PULLUP); // internal pullup input pin 21
    //Setting up interrupt
-  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 20 on moust Arduino.
   attachInterrupt(3, ai0, RISING);
    
-  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 21 on moust Arduino.
   attachInterrupt(2, ai1, RISING);
-  }
+}
    
-  void loop() {
-  digitalWrite(dirPin1, HIGH);
-
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin1, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin1, LOW);
-    delayMicroseconds(2000);
-  }
-  delay(1000); // Wait a second
-
-  digitalWrite(dirPin1, LOW);
-
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin1, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin1, LOW);
-    delayMicroseconds(1000);
-  }
-  delay(1000); // Wait a second
-  
-  // Send the value of counter
+void loop() {
+// Send the value of counter
   if( counter != temp ){
-  Serial.println (counter);
-  temp = counter;
+    Serial.println (counter);
+    temp = counter;
   }
-  }
-   
-  void ai0() {
-  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if(digitalRead(3)==LOW) {
-  counter++;
+}
+ 
+void ai0() {
+  // ai0 is activated if DigitalPin nr 20 is going from LOW to HIGH
+  // Check pin 21 to determine the direction
+  if(digitalRead(21)==LOW) {
+    counter++;
   }else{
-  counter--;
+    counter--;
   }
-  }
+}
    
-  void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if(digitalRead(2)==LOW) {
+void ai1() {
+  // ai0 is activated if DigitalPin nr 21 is going from LOW to HIGH
+  // Check with pin 20 to determine the direction
+  if(digitalRead(20)==LOW) {
   counter--;
   }else{
   counter++;
   }
-  }
+}
--- motor_moves_encoder/motor_moves_encoder.ino
+++ motor_moves_encoder/motor_moves_encoder.ino
@@ -10,7 +10,6 @@ void setup() {
   Serial.begin (9600);
 
   pinMode(20, INPUT_PULLUP); // internal pullup input pin 2 
-  
   pinMode(21, INPUT_PULLUP); // internal pullup input pin 3
    //Setting up interrupt
   //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
@@ -18,54 +17,56 @@ void setup() {
    
   //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
   attachInterrupt(2, ai1, RISING);
-  }
+}
    
-  void loop() {
-  digitalWrite(dirPin1, HIGH);
-
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin1, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin1, LOW);
-    delayMicroseconds(2000);
-  }
-  delay(1000); // Wait a second
-
-  digitalWrite(dirPin1, LOW);
-
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin1, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin1, LOW);
-    delayMicroseconds(1000);
-  }
-  delay(1000); // Wait a second
+void loop() {
+  
   
   // Send the value of counter
   if( counter != temp ){
-  Serial.println (counter);
-  temp = counter;
+    Serial.println (counter);
+    temp = counter;
   }
+  
+  digitalWrite(dirPin1, HIGH);
+  
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin1, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin1, LOW);
+    delayMicroseconds(2000);
   }
+  delay(1000); // Wait a second
+  
+  digitalWrite(dirPin1, LOW);
+  
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin1, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin1, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000); // Wait a second
+}
    
-  void ai0() {
+void ai0() {
   // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
   // Check pin 3 to determine the direction
-  if(digitalRead(3)==LOW) {
-  counter++;
+  if(digitalRead(21)==LOW) {
+    counter++;
   }else{
-  counter--;
+    counter--;
   }
-  }
+}
    
-  void ai1() {
+void ai1() {
   // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
   // Check with pin 2 to determine the direction
-  if(digitalRead(2)==LOW) {
-  counter--;
+  if(digitalRead(20)==LOW) {
+    counter--;
   }else{
-  counter++;
-  }
-  }
+    counter++;
+  } 
+}
