892c8cd47059024d8316c5b66bf35be01a009545
meltanrm <89637384+meltanrm@users.noreply.github.com>
2022-04-01 17:23:34 +0100

    Update Joystick_graphs.ino
    Formatted code + added reference

--- Joystick_graphs/Joystick_graphs.ino
+++ Joystick_graphs/Joystick_graphs.ino
@@ -1,17 +1,19 @@
-void setup() {
-// initialize serial communication at 9600 bits per second:
-pinMode(A3, INPUT_PULLUP);
-Serial.begin(9600);
-}
-// the loop routine runs over and over again forever:
-void loop() {
-// read the input on analog pin 0:
-int p = analogRead(A0);
-int t = analogRead(A1);
-int r = analogRead(A2);
-int b = analogRead(A3);
+//The following code was adapted from https://www.crazy-logic.co.uk/archives/1053 (Accessed 1 April 2022)
 
-String st = String(p) + " " + String(t) + " " + String(r) + " " + String(b);
-Serial.println(st);
-delay (10);
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  pinMode(A3, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the inputs on analog pins
+  int x = analogRead(A0);
+  int y = analogRead(A1);
+  int z = analogRead(A2);
+  int btn = analogRead(A3);  
+
+  String st = String(x) + " " + String(y) + " " + String(z) + " " + String(btn);
+  Serial.println(st);
+  delay(10);
 }
